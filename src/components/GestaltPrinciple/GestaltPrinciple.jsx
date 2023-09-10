7c2aa8ee8340e848812754ffec88de2101ebff6b
nadavkatan <nadavkatan110@gmail.com>
2023-09-10 23:16:05 +0300

    Add Gestalt components

---
+++ src/components/GestaltPrinciple/GestaltPrinciple.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import DOMPurify from "dompurify";
+import "./styles.css";
+
+export const GestaltPrinciple = ({ principle }) => {
+  const sanitizedPrinciple = DOMPurify.sanitize(principle.principle);
+  return (
+    <div className="principle-container">
+      <p
+        className="principle"
+        dangerouslySetInnerHTML={{ __html: sanitizedPrinciple }}
+      />
+      {principle.parameters.length > 0 && (
+        <ul className="principle-params-list">
+          {principle.parameters.map((param, i) => (
+            <li key={i} className="principle-param">
+              {param}
+            </li>
+          ))}
+        </ul>
+      )}
+      {principle.figures.length > 0 &&
+        principle.figures.map((figure, i) => (
+          <div className="principle-figures">
+            <img src={figure.src} alt="figure" />
+            <p>{figure.text}</p>
+          </div>
+        ))}
+    </div>
+  );
+};
---
+++ src/components/MIPGPrinciple/MIPGPrinciple.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import DOMPurify from "dompurify";
+import "./styles.css";
+
+export const MIPGPrinciple = ({ principle }) => {
+  const sanitizedPrinciple = DOMPurify.sanitize(principle.principle);
+  return (
+    <div className="mipg-principle-container">
+      <h3 className="mipg-principle">{principle.principle}</h3>
+      <div className="mipg-principle-components">
+        {principle.components.length > 0 &&
+          principle.components.map((component, i) =>
+            i === principle.components.length - 1 ? (
+              <div key={`principle-component-${i}`} className="mipg-component">
+                <p>{component}</p>
+              </div>
+            ) : (
+              <div key={`principle-component-${i}`} className="mipg-component">
+                <p>{component}</p>
+                <p className="connecting-component">And,</p>
+              </div>
+            )
+          )}
+      </div>
+      {principle.videos.length > 0 && (
+        <div className="principle-videos">
+          {principle.videos.map((video, i) => (
+            <iframe
+              key={`principle-video-${i}`}
+              src={video.src}
+              title={video.src}
+            />
+          ))}
+        </div>
+      )}
+      {principle.figures.length > 0 && (
+        <div className="principle-figures">
+          {principle.figures.map((figure, i) => (
+            <img key={`principle-figure-${i}`} src={figure.src} alt="" />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
